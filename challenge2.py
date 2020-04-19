ad1788353a65c5f0211c921c38ddf1fcc5097a54
Animesh Singh <62958196+iamanimeshh@users.noreply.github.com>
2020-04-19 22:03:42 +0530

    Add files via upload

---
+++ challenge2.py
@@ -0,0 +1,11 @@
+# ask a user to enter their first name and store it in a variable
+# ask a user to enter their last name and store it in a variable
+# print their full name
+# Make sure you have a space between first and last name
+# Make sure the first letter of first name and last name is uppercase
+# Make sure the rest of the name is lowercase
+firstname=input('plz enter your first name')
+lastname= input('enter your last name')
+
+print(firstname.capitalize() )
+print(lastname.capitalize())
